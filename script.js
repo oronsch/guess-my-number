fd3de1dd8b13f472ee1319a0f0767d79becf114e
oron <78795961+oronsch@users.noreply.github.com>
2022-02-20 21:38:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-'use strict';
+"use strict"
 
-let randomNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomNumber);
+// Creating elements
+let randomNumber = Math.trunc(Math.random() * 20) + 1
 let score = 10;
 let highScore = 0;
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guessNum = Number(document.querySelector(".guessNum").value);
-  console.log(guessNum);
+const placeMessage = message => document.querySelector(".message").textContent = message;
+const buttonControl = control => document.querySelector(".check").style.display = control;
+const changeBg = color => document.querySelector("body").style.backgroundColor = color;
+const secretNum = number => document.querySelector(".number").textContent = number;
 
-  if (!guessNum) {
-    document.querySelector(".message").textContent = "❌ Invalid Number ❌";
-  } else if (guessNum === randomNumber) {
-    document.querySelector(".check").style.display = "none";
-    document.querySelector(".number").textContent = `🎉 ${randomNumber} 🎉`;
-    document.querySelector("body").style.backgroundColor = "#8aac52";
-    document.querySelector(".message").textContent = "👏 We Have A Winner 👏";
-
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector(".highscore").textContent = highScore;
+// onClick button to get value from the input
+document.querySelector('.check').addEventListener('click', function () {
+    const guessNum = Number(document.querySelector(".guessNum").value);
+    
+  // Conditions for random gussed number
+    if (!guessNum) placeMessage("❌ Invalid Number ❌");
+    else if (guessNum === randomNumber) {
+        secretNum(`🎉 ${randomNumber} 🎉`)
+        changeBg("#8aac52");
+        buttonControl('none');
+        placeMessage("👏 We Have A Winner 👏");
+       
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }    
+    } else {
+        score--;
+        document.querySelector(".score").textContent = score;
+        placeMessage( guessNum > randomNumber ? "👇 Number Is Lower 👇" : "👆 Number is Higher 👆" );
     }
-    console.log(score, "test");
-  } else {
-    score--;
-    document.querySelector(".score").textContent = score;
-    guessNum > randomNumber
-      ? (document.querySelector(".message").textContent =
-          "👇 Number Is Lower 👇")
-      : (document.querySelector(".message").textContent =
-          "👆 Number is Higher 👆");
-  }
+    
+
 });
-
-document.querySelector(".tryAgain").addEventListener("click", function () {
-  randomNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(randomNumber, "second");
-  document.querySelector("body").style.backgroundColor = "cadetblue";
-  document.querySelector(".score").textContent = score = 10;
-  document.querySelector(".message").textContent = "🎰 Start Guessing 🎰";
-  document.querySelector(".number").textContent = "?";
-  Number((document.querySelector(".guessNum").value = ""));
-  document.querySelector(".check").style.display = "inline";
-});
-
-
+// Try again and restart the game
+document.querySelector('.tryAgain').addEventListener('click', function () {
+    randomNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.score').textContent = score = 10;
+    Number((document.querySelector(".guessNum").value = ""));
+    secretNum('?')
+    changeBg("cadetblue");
+    buttonControl("inline");
+    placeMessage('🎰 Start Guessing 🎰')
+   ;
+})
